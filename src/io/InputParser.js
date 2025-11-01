26303b2ed12de4aca6b5cb81595c38aa71a920d0
salmonco <xkqjsslsek80@naver.com>
2025-11-01 18:24:46 +0900

    feat(InputParser): 로또 번호, 보너스 번호를 숫자로 바꾸기

--- src/io/InputParser.js
+++ src/io/InputParser.js
@@ -2,5 +2,6 @@ export const InputParser = {
   parsePrice: (input) => {},
 
   splitByComma: (input) => input.split(","),
-  // 로또 번호, 보너스 번호를 숫자로 바꾸기
+
+  parseToNumbers: (inputs) => inputs.map(Number),
 };
